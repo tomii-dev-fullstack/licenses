c79477ced9d87e753b3ea0e1472d39eb24b0ea39
tomiidev <tomasruglio21@gmail.com>
2025-02-06 16:29:32 -0300

    tmp

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ import { send } from "./nodemailer/config.js";
 config();
 
 const app = express();
-app.use(cors({ origin: "*", methods: "GET, POST, PUT, DELETE, OPTIONS", credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173", "https://dashboard-gabiela.vercel.app"], methods: "GET, POST, PUT, DELETE, OPTIONS",credentials: true }));
 /* app.options('*', cors()); */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
