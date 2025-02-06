8adddd6a11715e47da24d499ecd0dcca587c6ae3
tomiidev <tomasruglio21@gmail.com>
2025-02-06 17:11:02 -0300

    aws

--- api/s3/s3.js
+++ api/s3/s3.js
@@ -15,7 +15,7 @@ const client = new S3Client({
     region: process.env.AWS_BUCKET_REGION,
     credentials: {
         accessKeyId: process.env.AWS_PUBLIC_KEY,
-        secretAccessKey: process.env.AWS_SECRET_KEY,
+        secretAccessKey: process.env.MY_AWS_SECRET_KEY,
     }
 });
 //v   pt      c                    
