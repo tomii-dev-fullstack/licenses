b7c704afe8790b2e01ebe5e30b77c942dee10769
tomiidev <tomasruglio21@gmail.com>
2025-03-07 20:38:23 -0300

    a

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ config();
 const app = express();
 app.use(cors({ origin: ["http://localhost:5173", "https://dashboard-gabiela.vercel.app"], methods: "GET, POST, PUT, DELETE, OPTIONS",credentials: true }));
 /* app.options('*', cors()); */
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use((err, req, res, next) => {
