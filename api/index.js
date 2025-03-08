74ede292b246afe87de75f788b8d0b67a45bf7f8
tomiidev <tomasruglio21@gmail.com>
2025-03-08 02:28:31 -0300

    route origin ecommercc

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ import { send } from "./nodemailer/config.js";
 config();
 
 const app = express();
-app.use(cors({ origin: ["http://localhost:5173","http://localhost:5174", "https://dashboard-gabiela.vercel.app"], methods: "GET, POST, PUT, DELETE, OPTIONS",credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173","http://localhost:5174", "https://dashboard-gabiela.vercel.app", "https://ecommerce-gabriela.vercel.app"], methods: "GET, POST, PUT, DELETE, OPTIONS",credentials: true }));
 /* app.options('*', cors()); */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
