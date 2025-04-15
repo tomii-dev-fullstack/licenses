6d68e6f430a21c71f795d4fabd0da60f9572d290
tomii-dev-fullstack <tomiiruglio98@gmail.com>
2025-04-15 11:57:30 -0300

    port changed

--- api/routes/route.js
+++ api/routes/route.js
@@ -21,7 +21,7 @@ async function startServer() {
   server.applyMiddleware({ app, path: '/graphql' });
 
   // Iniciar el servidor Express
-  const port = 4000;
+  const PORT = process.env.PORT || 4000;
   app.listen(port, (req, res) => {
     console.log(`🚀 Server running on http://localhost:${port}`);
     console.log(`GraphQL playground available at http://localhost:${port}${server.graphqlPath}`);
